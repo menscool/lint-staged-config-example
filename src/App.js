02a1b13bc4d79f7662d2c3c04931ffb9b19f92f4
menscool <menscool@yandex.ru>
2020-11-19 20:33:57 +0300

    Added testing stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+const OPTION_1 = 1;
+const OPTION_2 = 2;
+const OPTION_3 = 3;
+
 function App() {
+  let bottomText;
+  switch(2) {case OPTION_1: bottomText = 'option 1'; case OPTION_2: bottomText = 'option 2'; case OPTION_3: bottomText = 'option 3'}
   return (
     <div className="App">
       <header className="App-header">
